150bfc09adaafd036ae4bfd4a4d9cc403a8183db
barry <128946208+barrymamoudou@users.noreply.github.com>
2025-10-05 10:50:27 +0000

    Update view_roomtype.blade.php

--- resources/views/backend/allroom/roomtype/view_roomtype.blade.php
+++ resources/views/backend/allroom/roomtype/view_roomtype.blade.php
@@ -16,7 +16,7 @@
     <h6 class="mb-0 text-uppercase">Type Chambre (RoomType)</h6>
     <hr />
     <div class="card">
-        <div class="card-body">
+        <div class="card-body" >
             <div class="table-responsive">
                 <table id="example" class="table table-striped table-bordered" style="width:100%">
                     <thead>
