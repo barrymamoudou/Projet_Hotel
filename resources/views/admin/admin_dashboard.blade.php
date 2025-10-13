bbb20728bcaa8f1234b0b4aaef60c1fd0a0bbe58
barry <128946208+barrymamoudou@users.noreply.github.com>
2025-10-13 02:01:34 +0000

    update_room_sur_la_table_facility_mutlp

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -130,15 +130,15 @@
 
 <!--datatable JS-->
 
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-<script>
-   tinymce.init({
-     selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
-     plugins: 'powerpaste advcode table lists checklist',
-     toolbar: 'undo redo | blocks| bold italic | bullist numlist checklist | code | table'
-   });
-</script>
 
+<script src="{{ asset('tinymce.min.js') }}"></script>
+<script>
+  tinymce.init({
+    selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
+    plugins: 'code table lists',
+    toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+  });
+</script>
 
 <!-- 
 <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,7 @@ Route::middleware(['auth', 'roles:admin'])->group(function () {
     /// Room All Route 
     Route::controller(RoomController::class)->group(function(){
         Route::get('/edit/room/{id}', 'EditRoom')->name('edit.room');
+        Route::get('/update/room/{id}', 'UpdateRoom')->name('update.room');
         
     });
     
